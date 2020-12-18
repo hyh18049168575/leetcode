deb768c3a3fb0dc8c88c38e1c65d2926a0b02fc9
haozi <18049168575@qq.com>
2020-12-18 09:09:01 +0800

    4 day

---
+++ src/No389.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class No389 {
+    public static char solution(String s,String t){
+        char ans = t.charAt(t.length()-1);
+        for(int i = 0; i < s.length(); i++) {
+            ans ^= s.charAt(i);
+            ans ^= t.charAt(i);
+        }
+        return ans;
+    }
+}

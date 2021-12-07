5c043d628f9565a4a762e657878e74f782375f27
Haozi <18049168575@qq.com>
2021-12-08 04:05:46 +0800

    1208

--- src/leetcodeNumbers/No121.java
+++ src/leetcodeNumbers/No121.java
@@ -3,16 +3,14 @@ import java.lang.Math;
 
 public class No121 {
     public static int maxProfit(int[] prices) {
-        int max = 0;
+        int max;
         int min = 0;
         int dis = 0;
         for (int a = 0; a < prices.length-1; a++){
             if (prices[a]<prices[min]){
                 min = a;
             }
-            if (prices[a]>prices[max] || max > min){
-                max = a;
-            }
+            max = a+1;
             dis = Math.max(dis, prices[max]-prices[min]);
         }
         return dis;

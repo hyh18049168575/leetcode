15083d839c066af1ed158cdf8306e046b2a4ce91
Haozi <18049168575@qq.com>
2021-12-12 03:12:08 +0800

    12121

--- src/leetcodeNumbers/No283.java
+++ src/leetcodeNumbers/No283.java
@@ -1,7 +1,19 @@
 package leetcodeNumbers;
 
 public class No283 {
-    public void moveZeroes(int[] nums) {
-
+        public void moveZeroes(int[] nums) {
+            int a = 0;
+            int b = 0;
+            for (int i = 0;i<nums.length;i++){
+                if (nums[i] != 0){
+                    nums[a++] = nums[i];
+                }
+                if (nums[i] == 0) {
+                    b++;
+                }
+            }
+            for (int i = nums.length-1;i>= nums.length-b;i--){
+                nums[i] = 0;
+            }
+        }
     }
-}

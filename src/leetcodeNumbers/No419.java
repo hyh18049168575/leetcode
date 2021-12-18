849bda891f3813bb87b71973a8592fe444dcab0b
Yuhao Huang <18049168575@qq.com>
2021-12-18 17:56:42 +0800

    202112181740

--- src/leetcodeNumbers/No419.java
+++ src/leetcodeNumbers/No419.java
@@ -22,4 +22,16 @@ public class No419 {
         }
         return a;
     }
+    public static int countBattleships1(char[][] board) {
+        int m = board.length, n = board[0].length;
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0 && board[i - 1][j] == 'X') continue;
+                if (j > 0 && board[i][j - 1] == 'X') continue;
+                if (board[i][j] == 'X') ans++;
+            }
+        }
+        return ans;
+    }
 }

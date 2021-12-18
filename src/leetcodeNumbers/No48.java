08093e14e183c808790d001c53b2f0fe53b1082d
Haozi <18049168575@qq.com>
2021-12-19 05:46:21 +0800

    202112190546

--- src/leetcodeNumbers/No48.java
+++ src/leetcodeNumbers/No48.java
@@ -1,5 +1,16 @@
 package leetcodeNumbers;
 
 public class No48 {
-
+    public static void rotate(int[][] matrix) {
+        int a = matrix.length-1;
+        for (int i = 0;i < (a+1)/2; i++){
+            for (int j = 0;j < (a+2)/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[a-j][i];
+                matrix[a-j][i] = matrix[a-i][a-j];
+                matrix[a-i][a-j] = matrix[j][a-i];
+                matrix[j][a-i] = temp;
+            }
+        }
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,8 +1,8 @@
-import leetcodeNumbers.No419;
+import leetcodeNumbers.No48;
 
 public class main {
     public static void main(String[] args) {
-        char[][] a = {{'X'},{'X'},{'X'}};
-        No419.countBattleships(a);
+        int[][] a = {{1,2,3},{4,5,6},{7,8,9}};
+        No48.rotate(a);
     }
 }
